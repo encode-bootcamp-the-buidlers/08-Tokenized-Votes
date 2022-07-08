684818d9c4ce664436de5b286e7eef6be1322116
Bitcoinera <anagjordano@gmail.com>
2022-07-08 21:58:32 +0300

    add natspec to Token.sol

--- Project/contracts/Token.sol
+++ Project/contracts/Token.sol
@@ -6,27 +6,62 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
+/// @title A voting token that inherits from the ERC20Votes contract among others
+/// @author Encode Club
+/// @notice You can use this contract for learning about tokenized votes in Solidity
+/// @dev Enjoy learning 😎
+/// @custom:learning This is a contract with learning purposes only.
 contract MyToken is ERC20, AccessControl, ERC20Permit, ERC20Votes {
+    /// @dev Constant to identify the minter role
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
+    /// @dev We hardcode the token name and ticker. Everything else is inherited
     constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
     }
 
+    /// @notice Mint new tokens
+    /// @dev Only the contract deployer (who holds the minter role) can mint
+    /// @param to Address we are sending the minted tokens to
+    /// @param amount Amount of tokens to mint
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
 
     // The following functions are overrides required by Solidity.
 
-    function _afterTokenTransfer(address from, address to, uint256 amount)
-        internal
-        override(ERC20, ERC20Votes)
-    {
+    /**
+     * @dev Hook that is called after any transfer of tokens. This includes
+     * minting and burning.
+     *
+     * Calling conditions:
+     *
+     * - when `from` and `to` are both non-zero, `amount` of ``from``'s tokens
+     * has been transferred to `to`.
+     * - when `from` is zero, `amount` tokens have been minted for `to`.
+     * - when `to` is zero, `amount` of ``from``'s tokens have been burned.
+     * - `from` and `to` are never both zero.
+     *
+     * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
+     */
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Votes) {
         super._afterTokenTransfer(from, to, amount);
     }
 
+    /** @dev Creates `amount` tokens and assigns them to `account`, increasing
+     * the total supply.
+     *
+     * Emits a {Transfer} event with `from` set to the zero address.
+     *
+     * Requirements:
+     *
+     * - `account` cannot be the zero address.
+     */
     function _mint(address to, uint256 amount)
         internal
         override(ERC20, ERC20Votes)
@@ -34,6 +69,17 @@ contract MyToken is ERC20, AccessControl, ERC20Permit, ERC20Votes {
         super._mint(to, amount);
     }
 
+    /**
+     * @dev Destroys `amount` tokens from `account`, reducing the
+     * total supply.
+     *
+     * Emits a {Transfer} event with `to` set to the zero address.
+     *
+     * Requirements:
+     *
+     * - `account` cannot be the zero address.
+     * - `account` must have at least `amount` tokens.
+     */
     function _burn(address account, uint256 amount)
         internal
         override(ERC20, ERC20Votes)
